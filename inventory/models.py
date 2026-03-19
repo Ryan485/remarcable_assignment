87ec963cda4474d8e512ad4c1af0fbffd0b458ef
ryansong <song976431@gmail.com>
2026-03-18 20:58:36 -0700

    Add inventory models

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+# Tag model
+class Tag(models.Model):
+    name = models.CharField(max_length = 100)
+
+
+    def __str__(self):
+        return self.name
+
+# Product model
+class Product(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    # used PROTECT to avoid deleting categories that still have products in them
+    category = models.ForeignKey(Category, on_delete = models.PROTECT)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    tags = models.ManyToManyField(Tag)
+
+
+    def __str__(self):
+        return self.name
