023cac0938381ac04c981706073cc8dc6b4f7ca8
ryansong <song976431@gmail.com>
2026-03-19 23:42:16 -0700

    Add docstrings and comments

--- inventory/models.py
+++ inventory/models.py
@@ -1,7 +1,13 @@
+# Author : Ryan Song
+
 from django.db import models
 
-# Category model
+
 class Category(models.Model):
+    """
+    Represents a product category in the inventory
+    """
+    
     name = models.CharField(max_length = 100)
 
     class Meta:
@@ -10,18 +16,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-# Tag model
 class Tag(models.Model):
+    """
+    Represents a tag used to label products in the inventory
+    """
     name = models.CharField(max_length = 100)
 
 
     def __str__(self):
         return self.name
 
-# Product model
 class Product(models.Model):
+    """
+    Represents a Product in the inventory
+    """
     name = models.CharField(max_length = 100)
     description = models.TextField()
+
     # used PROTECT to avoid deleting categories that still have products in them
     category = models.ForeignKey(Category, on_delete = models.PROTECT)
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+# Author: Ryan Song
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Product, Category, Tag
+
+
+def product_list(request):
+    """
+    Display a list of products with optional search and filter functionality.
+    :param request: HTTP request object containing search and filter parameters
+    :precondition: request must be a valid HTTP GET request
+    :postcondition: returns filtered products based on search/category/tag inputs
+    :return: rendered HTML template with filtered products, categories and tags
+    """
+    
+    # fetch all products, tags, and categories from the database to populate the dropdowns
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+
+    search = request.GET.get('search')
+    category = request.GET.get('category')
+    tag = request.GET.get('tags')
+
+    if search:
+        products = products.filter(description__contains = search)
+
+    if category:
+        products = products.filter(category__id = category)
+
+    if tag:
+        products = products.filter(tags__id = tag)
+
+    return render(request, 'index.html', {
+        'products' : products,
+        'categories' : categories,
+        'tags' : tags
+        })
